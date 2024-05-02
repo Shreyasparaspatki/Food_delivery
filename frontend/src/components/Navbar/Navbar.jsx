c305a9a6dda7390c30c42ecb1513b7e15c32dbf1
shreyasparasptki <shreyasparaspatki25@gmail.com>
2024-05-02 19:13:23 +0530

    logo of website is done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 
+import { assets } from '../../assets/assets'
 import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        
+        <img src={assets.logo} alt="logo" />
     </div>
   )
 }
