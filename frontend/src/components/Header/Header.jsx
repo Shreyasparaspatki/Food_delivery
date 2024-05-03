84d80d39ded53e592be9b84314776d986b8f8412
shreyasparasptki <shreyasparaspatki25@gmail.com>
2024-05-03 16:56:52 +0530

    setup is done for home page

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order your Favourite food Here </h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, rem!</p>
+        <button>view Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
